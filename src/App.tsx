1a13dd0becb261000765c73dca7d348116bb3bd8
Nathan <nathc@udel.edu>
2024-02-13 22:28:36 -0500

    Added Hellow World

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript ft NATHAN CHEN
+                UD CISC275 with React Hooks and TypeScript ft NATHAN CHEN Hello
+                World
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                automatically reload. Hello World
             </p>
         </div>
     );

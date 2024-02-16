86706163b1c663eba923b3254e1e8ced17be3c5c
Nathan <nathc@udel.edu>
2024-02-15 21:30:29 -0500

    Added Fish

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import pic from "../Fishhh.jpg";
 
 function App(): JSX.Element {
     return (
@@ -12,6 +13,7 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World
             </p>
+            <img src={pic} width="100" height="50" />
         </div>
     );
 }

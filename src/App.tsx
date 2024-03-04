bd8715012a9fe00a9bbab5312c3c3f8583ab3838
Nathan <nathc@udel.edu>
2024-03-04 15:03:19 -0500

    refreshed the App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,18 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
-import pic from "./Fishhh.jpg";
-import { Button, Col, Container, Row } from "react-bootstrap";
-=======
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
->>>>>>> upstream/task-state
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript ft NATHAN CHEN Hello
-                World
+                UD CISC275 with React Hooks and TypeScript
             </header>
-<<<<<<< HEAD
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Hello World
-            </p>
-            <h1>WOAH HEADER???</h1>
-            <img src={pic} alt="FISH PIC PLEASE??" width="100" height="50" />
-            <ol>
-                <li>GAAHHH</li>
-                <li>EUGHHH</li>
-                <li>BAHHHH</li>
-            </ol>
-            <Button onClick={() => console.log("Hello World!")}>
-                Log Hello World!
-            </Button>
-            <Container>
-                <Row>
-                    <Col>
-                        <div className="App-rectangle"></div>
-                    </Col>
-                    <Col>
-                        <div className="App-rectangle"></div>
-                    </Col>
-                </Row>
-            </Container>
-=======
             <hr></hr>
             <Counter></Counter>
             <hr />
@@ -57,7 +25,6 @@ function App(): JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
->>>>>>> upstream/task-state
         </div>
     );
 }

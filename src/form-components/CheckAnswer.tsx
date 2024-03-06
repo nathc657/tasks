29b5b3ca7c7c10f61b26219a0da6cec7cdacc0f6
Nathan <nathc@udel.edu>
2024-03-06 15:43:59 -0500

    Check Answer now passing tests :D

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,33 @@
 import React, { useState } from "react";
+import { FormControl, FormGroup, FormLabel } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    //Do the thing like the instructions said
+    const [givenAnswer, setGivenAnswer] = useState<string>("");
+    const [isCorrect, setCorrect] = useState<string>("❌");
+
+    //Make a function like the BLESSED TOME did
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        const answer = event.target.value;
+        setGivenAnswer(answer);
+        if (answer === expectedAnswer) {
+            setCorrect("✔️");
+        } else {
+            setCorrect("❌");
+        }
+    }
+
     return (
         <div>
-            <h3>Check Answer</h3>
+            <FormGroup controlId="formInputAnswer">
+                <FormLabel>Enter Answer: </FormLabel>
+                <FormControl value={givenAnswer} onChange={updateAnswer} />
+            </FormGroup>
+            <h3>{isCorrect}</h3>
         </div>
     );
 }

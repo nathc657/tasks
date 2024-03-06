db16e5332d9322c11ee812a04732accb00da2cad
Nathan <nathc@udel.edu>
2024-03-06 16:32:50 -0500

    We done did the GiveAttempts one wooo

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,8 +1,43 @@
 import React, { useState } from "react";
+import { FormControl, FormGroup } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    //Do as the task commands
+    const [totalAttempts, setTotalAttempts] = useState<number>(3);
+    const [requestedAttempts, setRequestedAttempts] = useState<number>(0);
+
+    //Function for Requesting More Attempts
+    function requestAttempts(event: React.ChangeEvent<HTMLInputElement>) {
+        const userInput = event.target.value;
+        if (Number(userInput)) {
+            setRequestedAttempts(Number(userInput));
+        }
+    }
+
     return (
         <div>
+            <h1>Attempts Left: {totalAttempts}</h1>
+            <FormGroup controlId="formAttemptRequest">
+                <FormControl
+                    type="number"
+                    value={requestedAttempts}
+                    onChange={requestAttempts}
+                />
+            </FormGroup>
+            <Button
+                onClick={() => setTotalAttempts(totalAttempts - 1)}
+                disabled={totalAttempts <= 0}
+            >
+                use
+            </Button>
+            <Button
+                onClick={() =>
+                    setTotalAttempts(totalAttempts + requestedAttempts)
+                }
+            >
+                gain
+            </Button>
             <h3>Give Attempts</h3>
         </div>
     );
